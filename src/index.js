1ae994269a9457514c7b83005272bd388e50cd7f
bberak <bberak@gmail.com>
2019-03-09 14:36:46 +1100

    Moved index.js to the src folder and bumped version to 0.11.3

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import GameLoop from "./GameLoop";
+import GameEngine from "./GameEngine";
+import DefaultTouchProcessor from "./DefaultTouchProcessor";
+import DefaultTimer from "./DefaultTimer";
+
+export {
+	GameLoop,
+	GameLoop as BasicGameLoop,
+	GameEngine,
+	GameEngine as ComponentEntitySystem,
+	GameEngine as ComponentEntitySystems,
+	DefaultTouchProcessor,
+	DefaultTimer
+};
+
